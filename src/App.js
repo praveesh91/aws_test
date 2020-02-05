126968ddead40762249a312d380d38688b8569e4
Praveesh <praveesh@webenza.com>
2020-02-05 14:41:22 +0530

    [2020-02-05 14:41:22] test commit to see updates in aws amplify app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first commit to test aws amplify app
         </p>
         <a
           className="App-link"
